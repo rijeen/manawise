ea4aeaea751e50d40115d35e52d12932ddde7af9
Rick Jensen <rick@linkin.se>
2020-03-10 13:50:57 +0100

    minor fixes

--- src/react/build/index.php
+++ src/react/build/index.php
@@ -1,27 +1,17 @@
-<?php
-
-define('WP_USE_THEMES', false);
-
-require('./wp/core/wp-blog-header.php');
-
-?>
-
 <!DOCTYPE html>
 <html lang="sv">
 <head>
 	<meta charset="UTF-8" />
 	<title></title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="/style.<?php echo filemtime('style.min.css'); ?>.min.css" />
+	<link rel="stylesheet" href="style.<?php echo filemtime('style.min.css'); ?>.min.css" />
 	<link rel="icon" href="assets/img/favicon.png" type="image/x-icon">
-    <script>
-        var _nonce = "<?php echo wp_create_nonce( 'wp_rest' ); ?>";
-    </script>
+    <meta name="google" content="notranslate" />
 </head>
 <body id="body">
-<div id="root">
+<div id="root" class="notranslate">
 
 </div>
-<script src="/bundle.<?php echo filemtime('bundle.js'); ?>.js"></script>
+<script src="bundle.<?php echo filemtime('bundle.js'); ?>.js"></script>
 </body>
 </html>
